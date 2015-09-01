bcf6389611841f24a8679184dcb25ffd74a7b9dd
Matias Ali <mali@fpasoftware.com.ar>
2015-09-01 16:40:29 -0300

    Se agrega clase para loguear errores/informaciones/warning en archivo de texto

--- src/main/Main.cpp
+++ src/main/Main.cpp
@@ -2,10 +2,15 @@
 #include <fstream>
 #include <string>
 #include <vector>
-#include "ParserYAML/ParserYAML.h"
+#include "ParserYAML\ParserYAML.h"
+#include "Log\Logger.h"
 
 int main(int argc, char* args[]) {
 
+	Logger::getInstance()->writeError("Error");
+	Logger::getInstance()->writeInformation("Info");
+	Logger::getInstance()->writeWarning("Warning");
+	
 	//	test parser
 	ParserYAML* parser = new ParserYAML("config.yaml"); //TODO: Actualizar la ruta del archivo.
 	parser->parse();
