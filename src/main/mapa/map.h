186ed088c4a3fa0ee4c67edcbfb608b9bfa8b07b
Fiona Gonzalez Lisella <fgonzalez@aquadize.com>
2015-09-08 11:15:39 -0300

    Estructura de mapa para el modelo

--- src/main/mapa/map.h
+++ src/main/mapa/map.h
@@ -2,14 +2,23 @@
 #define SRC_MAIN_MAP_MAP_H_
 //-----------------------------------------------------------------------------
 #include <string>
+#include <vector>
+//-----------------------------------------------------------------------------
+#include "../entidades/entity.h"
+#include "../gfx/sprite_sheet.h"
 //-----------------------------------------------------------------------------
 class Map {
 
 private:
+	std::vector<Entity*> entities;
+	std::vector<SpriteSheet*> spriteSheets;
 
 public:
 	Map();
 	virtual ~Map();
+
+public:
+	void factoryMethod();
 };
 //-----------------------------------------------------------------------------
 #endif /* SRC_MAIN_MAP_MAP_H_ */
