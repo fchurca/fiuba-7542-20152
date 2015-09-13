5089c263b57bd8349d746f81c1a535f04eac6d48
Matias Ali <mali@fpasoftware.com.ar>
2015-09-13 00:37:11 -0300

    Modificacion SpriteSheet

--- src/main/gfx/sprite_sheet.cpp
+++ src/main/gfx/sprite_sheet.cpp
@@ -57,43 +57,6 @@ bool SpriteSheet::loadTexture( SDL_Renderer* renderer ){
 	return texture != NULL;
 }
 
-//void SpriteSheet::render( int x, int y, SDL_Renderer* renderer ){
-//	//	TODO: EL RENDER DEBE RECIBIR LA ENTIDAD PARA OBTENER LA UBICACION
-//
-//	//	Conversion isometrica - TODO: PONER LAS CONVERSIONES EN OTRA CLASE
-//	int screenX = ((x / 2) - ((y * TILE_WIDTH_DEFAULT) / (TILE_HEIGHT_DEFAULT * 2)));
-//	int screenY = (((x * TILE_HEIGHT_DEFAULT) / (TILE_WIDTH_DEFAULT * 2)) + (y / 2));
-//
-//	//	Ubicacion donde dibujar
-//	SDL_Rect renderQuad = { screenX + ANCHO_DEFAULT/2, screenY, TILE_WIDTH_DEFAULT, TILE_HEIGHT_DEFAULT };
-//
-//	//	Parte de la imagen a levantar - TODO: VER DE DONDE LEVANTARLO
-//	SDL_Rect clip = { 0, 0, TILE_WIDTH_DEFAULT, TILE_HEIGHT_DEFAULT };
-//
-//	//	Dibujado
-//	SDL_RenderCopy( renderer, getLoadedTexture( renderer ), &clip, &renderQuad );
-//}
-
-//void SpriteSheet::dibujarAnimado (int x, int y, SDL_Renderer* renderer ){
-//	//	TODO: EL RENDER DEBE RECIBIR LA ENTIDAD PARA OBTENER LA UBICACION
-//
-//	//	Conversion isometrica - TODO: PONER LAS CONVERSIONES EN OTRA CLASE
-//	int screenX = ((x / 2) - ((y * TILE_WIDTH_DEFAULT) / (TILE_HEIGHT_DEFAULT * 2)));
-//	int screenY = (((x * TILE_HEIGHT_DEFAULT) / (TILE_WIDTH_DEFAULT * 2)) + (y / 2));
-//
-//	//	Ubicacion donde dibujar
-//	SDL_Rect renderQuad = { screenX + ANCHO_DEFAULT/2, screenY, ancho_sprite, alto_sprite };
-//
-//	//	Parte de la imagen a levantar - TODO: VER DE DONDE LEVANTARLO
-//	int image = fps%total_sprites;
-//	SDL_Rect clip = { image * ancho_sprite, 0, ancho_sprite, alto_sprite };
-//
-//	//	Dibujado
-//	SDL_RenderCopy( renderer, getLoadedTexture( renderer ), &clip, &renderQuad );
-//	fps++; // uso fps como contador de imagen xq falta estado del modelo
-//}
-
-
 void SpriteSheet::render( int x, int y, int frame, Directions direction, SDL_Renderer* renderer ){
 
 	//	Conversion isometrica - TODO: PONER LAS CONVERSIONES EN OTRA CLASE
@@ -103,7 +66,7 @@ void SpriteSheet::render( int x, int y, int frame, Directions direction, SDL_Ren
 	//	Ubicacion donde dibujar
 	SDL_Rect renderQuad = { screenX - pixel_ref_x + ANCHO_DEFAULT/2, screenY - pixel_ref_y, ancho_sprite, alto_sprite };
 
-	int image = fps % total_sprites;
+	int image = fps % total_sprites;	//aca se debe usar el frame actual
 
 	//	Parte de la imagen a levantar - TODO: VER DE DONDE LEVANTARLO
 	SDL_Rect clip = { image * ancho_sprite, direction * alto_sprite, ancho_sprite, alto_sprite };
