36ac9c6686f6b46bb06fcff76ca56e2d9c9e7da9
Fiona Gonzalez Lisella <fgonzalezlisella@gmail.com>
2015-10-06 08:36:34 -0300

    Socket para windows

--- src/main/server/src/socket/socket.cpp
+++ src/main/server/src/socket/socket.cpp
@@ -1,9 +1,26 @@
 //-----------------------------------------------------------------------------
 #include "socket.h"
 //-----------------------------------------------------------------------------
-Socket::Socket() {
-	// TODO Auto-generated constructor stub
+Socket::Socket(unsigned int port)
+:status(false)
+{
+	int error = WSAStartup (0x0202, &wsaData);   // Fill in WSA info
+	SOCKADDR_IN addr; // The address structure for a TCP socket
 
+	if (error)
+		exit(1);
+
+	//Did we get the right Winsock version?
+	if (wsaData.wVersion != 0x0202)
+	{
+		WSACleanup(); //Clean up Winsock
+		exit(1);
+	}
+
+	addr.sin_family = AF_INET;      // Address family
+	addr.sin_port = htons (port);   // Assign port to this socket
+	addr.sin_addr.s_addr = htonl (INADDR_ANY);
+	sockfd = socket (AF_INET, SOCK_STREAM, IPPROTO_TCP); // Create socket
 }
 //-----------------------------------------------------------------------------
 Socket::~Socket() {
--- src/main/server/src/socket/socket.h
+++ src/main/server/src/socket/socket.h
@@ -2,27 +2,36 @@
 #ifndef SRC_SOCKET_SOCKET_H_
 #define SRC_SOCKET_SOCKET_H_
 //-----------------------------------------------------------------------------
-#include <string>
-//-----------------------------------------------------------------------------
 /*
  * Interfaz comun para utilzar sockets. *
  */
 //-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+#include <string>
+//-----------------------------------------------------------------------------
+#ifdef _WIN32
+	#include <windows.h>
+	#include <winsock2.h>
+	#include <ws2tcpip.h>
+
+#endif
+
+
 class Socket {
 
 private:
-
+	WSADATA wsaData;
 	int sockfd;							// Filedescriptor del socket.
-	//struct sockaddr_in miDir;			// Dirección del socket.
-	//struct sockaddr_in destinoDir;		// Dirección del socket destino.
+	struct addrinfo serv_addr;
+	//analizar si es necesario
+	struct sockaddr_in cli_addr;
 	bool status;
 
 public:
-	Socket();
+	Socket(unsigned int port);
 	virtual ~Socket();
-public:
 
-	void create();
+public:
 	void connect(std::string hostIP, int hostPort);
 	void listen(int maxConnections, int port, std::string ip);
 	Socket* accept();
