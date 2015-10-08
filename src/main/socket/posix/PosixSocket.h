dc3976a56ae44cc006da2a05811754802c859144
Fiona Gonzalez Lisella <fgonzalez@aquadize.com>
2015-10-08 17:24:58 -0300

    Interfaz para posix socket

--- src/main/socket/posix/PosixSocket.h
+++ src/main/socket/posix/PosixSocket.h
@@ -6,6 +6,13 @@ class PosixSocket: public Socket {
 public:
 	PosixSocket();
 	virtual ~PosixSocket();
+public:
+	void Connect(std::string hostIP, int hostPort);
+	bool Listen(int maxConnections);
+	bool Accept();
+	int Send(const void* data, int dataLenght);
+	int Recv(const void* data, int dataLenght);
+	bool IsActive();
 
 
 };
