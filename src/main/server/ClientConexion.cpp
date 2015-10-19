e0db65d6902d469fc9a23ee96d73198b453b5329
Fiona Gonzalez Lisella <fgonzalez@aquadize.com>
2015-10-18 21:51:25 -0300

    agrego la clase que mantiene la funcionalidad de socket para cada cliente

---
+++ src/main/server/ClientConexion.cpp
@@ -0,0 +1,11 @@
+#include "ClientConexion.h"
+//-----------------------------------------------------------------------------
+ClientConexion::ClientConexion() {
+	// TODO Auto-generated constructor stub
+
+}
+//-----------------------------------------------------------------------------
+ClientConexion::~ClientConexion() {
+	// TODO Auto-generated destructor stub
+}
+//-----------------------------------------------------------------------------
---
+++ src/main/server/ClientConexion.h
@@ -0,0 +1,12 @@
+//-----------------------------------------------------------------------------
+#ifndef SRC_MAIN_SERVER_CLIENTCONEXION_H_
+#define SRC_MAIN_SERVER_CLIENTCONEXION_H_
+//-----------------------------------------------------------------------------
+class ClientConexion {
+public:
+	ClientConexion();
+	virtual ~ClientConexion();
+};
+//-----------------------------------------------------------------------------
+#endif /* SRC_MAIN_SERVER_CLIENTCONEXION_H_ */
+//-----------------------------------------------------------------------------
