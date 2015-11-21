88a7146461fa50babb356132121e90a35764a25e
alileandro93 <ali.leandro.93@gmail.com>
2015-11-21 01:47:30 -0300

    Comienzo command menu

--- src/main/gfx/command_menu.cpp
+++ src/main/gfx/command_menu.cpp
@@ -17,12 +17,23 @@ void CommandMenu::visit(Entity& entity) {
 }
 void CommandMenu::visit(Unit& entity) {
 	visit((Entity&)entity);
+	outText = outText + owner.completeLine("[] Ir", size.x);
+	outText = outText + owner.completeLine("[] Atacar", size.x);
+	outText = outText + owner.completeLine("[] Seguir", size.x);
+	outText = outText + owner.completeLine("[] Parar", size.x);
+
 }
 void CommandMenu::visit(Worker& entity) {
 	visit((Unit&)entity);
+	outText = outText + owner.completeLine("[] Reparar", size.x);
+	outText = outText + owner.completeLine("[] Recolectar", size.x);
+	outText = outText + owner.completeLine("[] Construir", size.x);
+	outText = outText + owner.completeLine("--> [] Posibles edificaciones", size.x);
 }
 void CommandMenu::visit(ProducerBuilding& entity) {
 	visit((Entity&)entity);
+	outText = outText + owner.completeLine("[] Producir", size.x);
+	outText = outText + owner.completeLine("--> [] Posibles productos", size.x);
 }
 
 void CommandMenu::draw() {
@@ -30,16 +41,15 @@ void CommandMenu::draw() {
 	SDL_Rect destinoFondoMenu = { (int)offset.x, (int)offset.y, (int)size.x, (int)size.y };
 	SDL_SetRenderDrawColor(owner.getRenderer(), 15, 15, 15, 255);
 	SDL_RenderFillRect(owner.getRenderer(), &destinoFondoMenu);
+	outText = "";
+	SDL_Color colorBlanco = { 255, 255, 255 };
 	if (owner.font) {
-		std::string texto = "";
-		SDL_Color colorBlanco = { 255, 255, 255 };
-		texto = owner.completeLine(owner.player.name, size.x);
-		for (auto r : owner.player.getResources()) {
-			texto = texto + owner.completeLine(r.first + "=" + std::to_string(r.second), size.x);
+		if (owner.getSelection().size() > 0) {
+			owner.getSelection().at(0)->visit(*this);
 		}
 		int access1, w1, h1;
 		Uint32 format1;
-		SDL_Surface * c1 = TTF_RenderText_Blended_Wrapped(owner.font, texto.c_str(), colorBlanco, (Uint32)(size.x));
+		SDL_Surface * c1 = TTF_RenderText_Blended_Wrapped(owner.font, outText.c_str(), colorBlanco, (Uint32)(size.x));
 		SDL_Texture * textureMenu1 = SDL_CreateTextureFromSurface(owner.getRenderer(), c1);
 
 		SDL_QueryTexture(textureMenu1, &format1, &access1, &w1, &h1);
