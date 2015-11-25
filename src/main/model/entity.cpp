87a4a6e321e72f7d0c3519338c557b938f1d0600
alileandro93 <ali.leandro.93@gmail.com>
2015-11-25 20:58:57 -0300

    Agrego algunas validaciones

--- src/main/model/entity.cpp
+++ src/main/model/entity.cpp
@@ -665,7 +665,7 @@ std::shared_ptr<Command> Unit::giveDefaultCommand(Entity& r) {
 }
 
 std::shared_ptr<Command> Unit::giveDefaultCommand(Unit& r) {
-	if (owner.getActive() && owner.name != r.owner.name)
+	if (owner.getActive() && (owner.name != r.owner.name))
 		return std::make_shared<AttackCommand>(r.getId(), getId());
 	else
 		return giveDefaultCommand((Entity&)r);
@@ -676,14 +676,14 @@ std::shared_ptr<Command> Building::giveDefaultCommand(Entity& r) {
 }
 
 std::shared_ptr<Command> Building::giveDefaultCommand(Unit& r) {
-	if (owner.getActive() && owner.name != r.owner.name)
+	if (owner.getActive() && (owner.name != r.owner.name))
 		return std::make_shared<AttackCommand>(r.getId(), getId());
 	else
 		return giveDefaultCommand((Entity&)r);
 }
 
 std::shared_ptr<Command> Building::giveDefaultCommand(Worker& r) {
-	if (owner.getActive() && owner.name != r.owner.name)
+	if (owner.getActive() && (owner.name != r.owner.name))
 		return giveDefaultCommand((Unit&)r);
 	else
 		return std::make_shared<RepairCommand>(r.getId(), getId());
@@ -694,7 +694,7 @@ std::shared_ptr<Command> Flag::giveDefaultCommand(Entity& r) {
 }
 
 std::shared_ptr<Command> Flag::giveDefaultCommand(Unit& r) {
-	if (owner.getActive() && owner.name != r.owner.name)
+	if (owner.getActive() && (owner.name != r.owner.name))
 		return std::make_shared<AttackCommand>(r.getId(), getId());
 	else
 		return giveDefaultCommand((Entity&)r);
